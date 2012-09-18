d6810d28bb5b41eed8f54a6924c2d54abfa3eecc
Dave Hulbert <dave1010@gmail.com>
2012-09-18 22:28:10 +0100

    Tweet "setservo 123" to move the servo to 123 degrees

--- serial_to_servo/serial.php
+++ serial_to_servo/serial.php
@@ -1,11 +1,14 @@
 #!/usr/bin/php
 <?php
 
+// find tweets matching "setservo [\d]{1,3}", then move the servo
+
 ini_set('display_errors', -1);
 
 define('SERIAL', '/dev/ttyACM0');
 
 $f = fopen(SERIAL, 'w+');
+$lastDegrees = 0;
 
 function setServo($degrees) {
 	global $f;
@@ -15,15 +18,28 @@ function setServo($degrees) {
 		$degrees = 170;
 	}
 
+	if ($degrees === $GLOBALS['lastDegrees']) {
+		return;
+	}
+
 	echo $degrees."\n";
 	fwrite($f, $degrees."\n");
-
 }
 
+
 while(true) {
-	$v = rand(1,170)."\n";
-	setServo($v);
-	usleep(500*1000);
+	$tweets = @json_decode(file_get_contents('http://search.twitter.com/search.json?q=setservo&result_type=recent'));
+
+	if (@$tweets->results[0]->text) {
+		$tweet = @$tweets->results[0]->text;
+		
+		$degrees = preg_match('/setservo ([\d]{1,3})/', $tweet, $matches);
+		if ($matches[1]) {
+			setServo($matches[1]);
+		}
+	}
+	
+	usleep(5*1000*1000);
 }
 
 
